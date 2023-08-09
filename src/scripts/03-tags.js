8dacd1a39cf6e17914efc3d79a51dcffc81a7a44
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-08-09 12:19:55 +0300

    update

--- src/scripts/03-tags.js
+++ src/scripts/03-tags.js
@@ -1,4 +1,34 @@
 let refs = {
   firstTagsList: document.querySelector('.tags-box[data-group="group-1"]'),
   secondTagsList: document.querySelector('.tags-box[data-group="group-2"]'),
+  btnElem: document.querySelector('.js-btn'),
 };
+
+refs.firstTagsList.addEventListener('click', onCheckboxListClick);
+refs.secondTagsList.addEventListener('click', onRadioListClick);
+refs.btnElem.addEventListener('click', sendData);
+
+function onCheckboxListClick(evt) {
+  if (evt.target === evt.currentTarget) return;
+  const liElem = evt.target.closest('li');
+  liElem.classList.toggle('active');
+}
+
+function onRadioListClick(evt) {
+  if (evt.target === evt.currentTarget) return;
+
+  const oldElem = refs.secondTagsList.querySelector('.active');
+  oldElem?.classList?.remove('active');
+
+  const liElem = evt.target.closest('li');
+  liElem.classList.toggle('active');
+}
+
+function sendData() {
+  const checkboxElems = refs.firstTagsList.querySelectorAll('.active');
+  const radioActive = refs.secondTagsList.querySelector('.active');
+
+  const tagList = [...checkboxElems].map(el => el.textContent.trim());
+  console.log(tagList);
+  console.log(radioActive?.textContent.trim());
+}
--- src/scripts/04-color-picker.js
+++ src/scripts/04-color-picker.js
@@ -1,10 +1,10 @@
 let colorPalette = [];
-const LENGTH = 5;
+const LENGTH = 10;
 
 function createPaletteItems() {
   const items = [];
   for (let i = 0; i < LENGTH; i++) {
-    let hex = getRangomColor();
+    let hex = getRandomColor();
     let color = {
       hex,
       rgb: hexToRgb(hex),
@@ -16,7 +16,7 @@ function createPaletteItems() {
   colorPalette = [...items];
 }
 
-function getRangomColor() {
+function getRandomColor() {
   return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
 }
 
@@ -34,7 +34,8 @@ function hexToRgb(hex) {
 }
 
 createPaletteItems();
-////////////////////////////////////////////////////////////////////////////
+
+//=====================================================
 
 const refs = {
   itemList: document.querySelector('.js-colors-box'),
@@ -42,18 +43,56 @@ const refs = {
   btnReloadColor: document.querySelector('.js-reload-color'),
   backdropElem: document.querySelector('.js-backdrop'),
 };
+//=====================================================
 
-////////////////////////////////////////////////////////////////////////////
+refs.btnReloadColor.addEventListener('click', () => {
+  createPaletteItems();
+  renderColors(colorPalette);
+});
 
-/* 
-nodeName
-<li class="color-item">
-    <button class="color-body style="background-color:...;"></button>
-    <div class="color-footer">
-        <div>HEX: ....</div>
-        <div>RGB: ....</div>
-        <div></div>
-    </div>
-</li>
+refs.itemList.addEventListener('click', evt => {
+  if (!evt.target.classList.contains('color-body')) return;
+  refs.modalElement.style.backgroundColor = evt.target.style.backgroundColor;
+  showModal();
+});
 
-*/
+refs.backdropElem.addEventListener('click', evt => {
+  if (evt.target === evt.currentTarget) hideModal();
+});
+
+function onModalKeyDown(evt) {
+  console.log(evt.key);
+  if (evt.key !== 'Escape') return;
+  hideModal();
+}
+
+//=====================================================
+function colorTemplate({ hex, rgb }) {
+  return `<li class="color-item">
+  <button class="color-body" style="background-color: ${hex}"></button>
+  <div class="color-footer">
+    <div>HEX: ${hex}</div>
+    <div>RGB: ${rgb}</div>
+    <div></div>
+  </div>
+</li>`;
+}
+
+function renderColors(colors) {
+  const markup = colors.map(colorTemplate).join('');
+  refs.itemList.innerHTML = markup;
+}
+
+renderColors(colorPalette);
+
+//=====================================================
+function showModal() {
+  document.body.classList.add('show-modal');
+  document.addEventListener('keydown', onModalKeyDown);
+}
+
+function hideModal() {
+  document.body.classList.remove('show-modal');
+  document.removeEventListener('keydown', onModalKeyDown);
+}
+//=====================================================
