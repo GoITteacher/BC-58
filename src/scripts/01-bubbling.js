107378d0ebbcef6bd3814c1d7ab0d9cb84f6f77c
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-08-09 11:16:48 +0300

    update

--- src/scripts/01-bubbling.js
+++ src/scripts/01-bubbling.js
@@ -8,7 +8,32 @@
 */
 
 let refs = {
-  parrent: document.querySelector('.js-parent'),
+  parent: document.querySelector('.js-parent'),
   child: document.querySelector('.js-child'),
   descendant: document.querySelector('.js-descendant'),
 };
+
+refs.parent.addEventListener('click', evt => {
+  console.log(evt.target, evt.currentTarget);
+});
+
+refs.child.addEventListener('click', evt => {
+  console.log(evt.target, evt.currentTarget);
+});
+
+refs.descendant.addEventListener('click', evt => {
+  console.log(1);
+});
+
+refs.descendant.addEventListener('click', evt => {
+  console.log(2);
+});
+
+refs.descendant.addEventListener('click', evt => {
+  console.log(3);
+  // evt.stopImmediatePropagation();
+});
+
+refs.descendant.addEventListener('click', evt => {
+  console.log(4);
+});
--- src/scripts/02-delegation.js
+++ src/scripts/02-delegation.js
@@ -1,3 +1,28 @@
 let refs = {
-  firstBoxElem: document.querySelector('.js-box'),
+  firstBoxElem: document.querySelector('.js-box-0'),
 };
+
+refs.firstBoxElem.addEventListener('click', onBtnListClick);
+
+/* function onBtnListClick(evt) {
+  if (evt.target === evt.currentTarget) return;
+  // console.log(evt.target.nodeName);
+  const btnElem = evt.target.closest('li').lastElementChild;
+  console.log(btnElem.textContent.trim());
+}
+ */
+
+/* function onBtnListClick(evt) {
+  if (evt.target.nodeName !== 'BUTTON' && evt.target.nodeName !== 'SPAN')
+    return;
+
+  console.log(evt.target.nodeName);
+} */
+
+function onBtnListClick(evt) {
+  const myTarget = evt.target.closest('.js-delegation-btn');
+
+  if (!myTarget) return;
+
+  console.log(evt.target.nodeName);
+}
